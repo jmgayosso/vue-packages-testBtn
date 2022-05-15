6a4f69e7015e99f62a90ffd816e9044a316288b0
Jose <josemaria@hashed.io>
2022-05-14 22:35:24 -0500

    inital commit

---
+++ dist/index.js
@@ -0,0 +1,40 @@
+/*!
+ * @jmgayosso/testBtn v1.0.0
+ * (c) jmgayosso
+ * Released under the ISC License.
+ */
+'use strict';
+
+var vue = require('vue');
+
+var script = {
+  name: 'testBtn',
+  methods: {
+    click: function click() {
+      alert('Clicked');
+    }
+  }
+};
+
+var _hoisted_1 = {
+  id: "container"
+};
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  return vue.openBlock(), vue.createElementBlock("div", _hoisted_1, [vue.createElementVNode("button", {
+    onClick: _cache[0] || (_cache[0] = function () {
+      return $options.click && $options.click.apply($options, arguments);
+    })
+  }, " This is a test!! ")]);
+}
+
+script.render = render;
+
+var index = {
+  install: function install(Vue, options) {
+    // Let's register our component globally
+    // https://vuejs.org/v2/guide/components-registration.html
+    Vue.component("test-btn", script);
+  }
+};
+
+module.exports = index;
---
+++ src/components/testBtn.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="container">
+    <button @click="click">
+        This is a test!!
+    </button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'testBtn',
+  methods: {
+    click () {
+      alert('Clicked')
+    }
+  }
+}
+</script>
